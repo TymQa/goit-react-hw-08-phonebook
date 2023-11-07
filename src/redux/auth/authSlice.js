a1d7c04d3b66d56a61ba0a7abf8bd3fe31da13ec
Tymur Qayoom <tymursheikh4576@gmail.com>
2023-11-07 20:06:58 +0200

    +

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -20,68 +20,69 @@ export const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {},
-  extraReducers: {
-    [registerUser.pending](state) {
-      state.isLoading = true;
-      state.error = null;
-    },
-    [registerUser.fulfilled](state, { payload }) {
-      state.isLoading = false;
-      state.isLoggedIn = true;
-      state.token = payload.token;
-      state.user = payload.user;
-      toast.success('Successfully registered!');
-    },
-    [registerUser.rejected](state, { payload }) {
-      state.isLoading = false;
-      state.error = payload;
-      toast.error('Something is wrong, try again!');
-    },
-    [loginUser.pending](state) {
-      state.isLoading = true;
-      state.error = null;
-    },
-    [loginUser.fulfilled](state, { payload }) {
-      state.isLoading = false;
-      state.isLoggedIn = true;
-      state.token = payload.token;
-      state.user = payload.user;
-      toast.success('Successfully logged in!');
-    },
-    [loginUser.rejected](state, { payload }) {
-      state.isLoading = false;
-      state.error = payload;
-      toast.error('Something is wrong, try again!');
-    },
-    [logoutUser.pending](state) {
-      state.isLoading = true;
-      state.error = null;
-    },
-    [logoutUser.fulfilled](state) {
-      state.isLoading = false;
-      state.isLoggedIn = false;
-      state.user = { name: null, email: null };
-      state.token = null;
-      toast.success('Successfully logged out! Waiting for you to come back!');
-    },
-    [logoutUser.rejected](state, { payload }) {
-      state.isLoading = false;
-      state.error = payload;
-      toast.error('Something is wrong, try again later!');
-    },
-    [fetchCurrentUser.pending](state) {
-      state.isLoadingUser = true;
-      state.error = null;
-    },
-    [fetchCurrentUser.fulfilled](state, { payload }) {
-      state.isLoadingUser = false;
-      state.isLoggedIn = true;
-      state.user = payload;
-    },
-    [fetchCurrentUser.rejected](state, { payload }) {
-      state.isLoadingUser = false;
-      state.error = payload;
-    },
+  extraReducers: builder => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(registerUser.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.isLoggedIn = true;
+        state.token = payload.token;
+        state.user = payload.user;
+        toast.success('Successfully registered!');
+      })
+      .addCase(registerUser.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        state.error = payload;
+        toast.error('Something is wrong, try again!');
+      })
+      .addCase(loginUser.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(loginUser.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.isLoggedIn = true;
+        state.token = payload.token;
+        state.user = payload.user;
+        toast.success('Successfully logged in!');
+      })
+      .addCase(loginUser.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        state.error = payload;
+        toast.error('Something is wrong, try again!');
+      })
+      .addCase(logoutUser.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(logoutUser.fulfilled, (state) => {
+        state.isLoading = false;
+        state.isLoggedIn = false;
+        state.user = { name: null, email: null };
+        state.token = null;
+        toast.success('Successfully logged out! Waiting for you to come back!');
+      })
+      .addCase(logoutUser.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        state.error = payload;
+        toast.error('Something is wrong, try again later!');
+      })
+      .addCase(fetchCurrentUser.pending, (state) => {
+        state.isLoadingUser = true;
+        state.error = null;
+      })
+      .addCase(fetchCurrentUser.fulfilled, (state, { payload }) => {
+        state.isLoadingUser = false;
+        state.isLoggedIn = true;
+        state.user = payload;
+      })
+      .addCase(fetchCurrentUser.rejected, (state, { payload }) => {
+        state.isLoadingUser = false;
+        state.error = payload;
+      });
   },
 });
 
